774c42f0f3c0bd93b7b41aa8142f90e131649509
ClariNerd617 <5322154+ClariNerd617@users.noreply.github.com>
2025-11-30 22:03:56 -0500

    more work done
    work done work done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # /// script
-# requires-python = ">=3.13"
 # dependencies = [
 #   "httpx",
+#   "feedparser",
 # ]
 # ///
+
 from httpx import AsyncClient, BasicAuth
 from os import environ
 import logging
@@ -13,6 +14,7 @@ import asyncio
 import xml.etree.ElementTree as ET
 from typing import List, Dict, Optional, IO
 from pathlib import Path
+import feedparser
 
 
 def parse_opml(source: str | Path | IO[str]) -> List[Dict[str, Optional[str]]]:
@@ -47,30 +49,28 @@ async def add_articles(urls: list[str], auth: BasicAuth, log: logging.Logger) ->
         base_url="https://www.instapaper.com/api/add", auth=auth
     ) as client:
         for url in urls:
+            log.info(f"Adding url {url}.")
             r = await client.post(data={"url": url})
             r.raise_for_status()
 
 
 def get_articles(feeds: list[dict[str, str]]) -> list[str]:
-    """
-    TODO Implement using feedparser\n
-    Will use GitHub Actions to update SQLite DB in repo as detailed in main() below.
-
-    feeds data object as follows
-    [
-        {
-            "url": url,
-            "last_checked: datetime.from_isoformat(datestring),
-        },
-    ]
-
-    ---
-    """
-
+    url_list = []
     for feed in feeds:
-        url = feed["url"]
-        last_checked = feed["last_checked"]
-        ...
+        feed_url = feed["url"]
+        last_checked = datetime.fromisoformat(feed["last_checked"])
+        """NOTE: at an unspecified date in the future:
+        feedparse will stop handling the http part of parsing feeds
+        (source: https://github.com/kurtmckee/feedparser/issues/199#issuecomment-570113456)
+        so I will need to add a get_feeds() function and parse the data here
+        On the plus side, that means I can async that part too.
+        On the minus side, this makes the async main() more complicated for now."""
+        feed_parsed = feedparser.parse(feed_url) # unofficially deprecated
+        feed_entries = feed_parsed.entries
+        for entry in feed_entries:
+            entry_date = datetime.fromisoformat(entry.published)
+            if entry_date - last_checked >= timedelta():
+                url_list.append(entry.link)
     return url_list
 
 
@@ -88,26 +88,29 @@ async def main() -> None:
         password=environ.get("INSTAPAPER_PASSWORD", ""),
     )
 
-    with open("./feeds.opml") as feedsfile:
-        result = parse_opml(feedsfile)
-    feedslist: list[str] = [feed[0].url for feed in result.feeds]
-    """ TODO Using SQLite, figure out a workflow for:
+    with open("feeds.opml") as feedsfile:
+        feeds = parse_opml(feedsfile)
+    feedslist: list[str] = [feed[0]["xml_url"] for feed in feeds]
+    """TODO Using SQLite, figure out a workflow for:
       - on first run:
         - create sqlite db in repo, populate with feed urls and current timestamp in isoformat
+        - only get the five most-recent urls in this case
       - on subsequent runs:
         - check sqlite db for feed urls and timestamps
         - update db with current timestamps
-      - determine whether first run based on existence checks
-      - feed resulting data structure of the form detailed in the docstring for get_articles() above
     """
+    # rwc mode is read-write-create. If the file doesn't exist, this'll create it.
+    con = sqlite3.connect("file:feeds.db?mode=rwc", uri=True)
+    # TODO last_checked set from db for each feed_url
     feeds = [
         {
             "url": url,
-            "last_checked": datetime.now(datetime.UTC).isoformat(),
+            "last_checked": "", 
         }
         for url in feedslist
     ]
     urls: list[str] = await get_articles(feeds=feeds)
+    # TODO DB UPDATE last_checked -> datetime.now(datetime.UTC).isoformat()
     await add_articles(urls=urls, auth=auth, log=log)
 
 
